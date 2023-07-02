764f31461ef2b10fc19223e1da75f2ff63401b44
hadep275 <hadep275@gmail.com>
2023-07-02 13:15:43 -0600

    Project Img added

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import "./project.css";
+import "./project.css"
+import ColorSharp from "../assets/img/color-sharp.png"
+import Project2 from "../assets/img/project2.png"
+import Project3 from "../assets/img/project3.png"
 const project = () => {
   return (
     <div class="project">
@@ -11,8 +14,8 @@ const project = () => {
           <div class="col-md-3">
             <div class="cardholder">
               <div class="card">
+              <img src={ColorSharp} />
                 <p class="desc">QuantumX</p>
-                <img src="" />
               </div>
             </div>
           </div>
@@ -21,7 +24,7 @@ const project = () => {
             <div class="cardholder">
               <div class="card">
                 <p class="desc">Indeygo</p>
-                <img src="" />
+                <img src={Project3} />
               </div>
             </div>
           </div>
@@ -30,7 +33,7 @@ const project = () => {
             <div class="cardholder">
               <div class="card">
                 <p class="desc">EveryChildPlays</p>
-                <img src="" />
+                <img src={Project2} />
               </div>
             </div>
           </div>
