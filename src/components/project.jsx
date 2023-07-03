b3b241bd3e8ca1fdbec8f6840b52e749346981f7
hadep275 <hadep275@gmail.com>
2023-07-02 23:41:17 -0600

    project made responsive

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -2,11 +2,26 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "./project.css"
-import ColorSharp from "../assets/img/color-sharp.png"
-import Project2 from "../assets/img/project2.png"
-import Project3 from "../assets/img/project3.png"
+import "./project.css";
+import ColorSharp from "../assets/img/color-sharp.png";
+import Project2 from "../assets/img/project2.png";
+import Project3 from "../assets/img/project3.png";
 const project = () => {
+  const [autoplay, setAutoplay] = React.useState(true);
+  const handleAutoplayToggle = () => {
+    setAutoplay(!autoplay);
+  };
+  const SampleNextArrow = (props) => {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{ ...style, display: "block", background: "red" }}
+        onClick={onClick}
+      />
+    );
+  };
+
   const settings = {
     dots: true,
     infinite: true,
@@ -15,33 +30,36 @@ const project = () => {
     slidesToScroll: 3,
     centerMode: true, // Center the active card
     centerPadding: "10px", // Space between cards
+    nextArrow: <SampleNextArrow />,
     responsive: [
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 1, // Display 1 card at a time on smaller screens
           slidesToScroll: 1, // Display 1 card at a time on smaller screens
-          infinite: true, 
-          dots: true, 
+          infinite: true,
+          dots: true,
+          slidesToShow: 1, 
           centerMode: true, // Center the active card
           centerPadding: "40px", // Adjust spacing for smaller screens
         },
       },
     ],
-
+    autoplay: true, // Enable autoplay
+    autoplaySpeed: 3000, // Set autoplay speed (in milliseconds)
+    pauseOnHover: true, // Pause autoplay on hover
   };
   return (
     <div class="project">
       <div class="container">
         <h1>Projects</h1>
       </div>
-      <div class="container">
-      <Slider {...settings}>
-        {/* <div class="row"> */}
+      <div class="carousel-container">
+        <Slider {...settings} autoplay={autoplay} autoplaySpeed={3000}>
           <div class="col-md-3">
             <div class="cardholder">
               <div class="card">
-              <img src={ColorSharp} />
+                <img src={ColorSharp} />
                 <p class="desc">QuantumX</p>
               </div>
             </div>
@@ -73,8 +91,15 @@ const project = () => {
               </div>
             </div>
           </div>
-        {/* </div> */}
         </Slider>
+        <div>
+          <button
+            className="slick-autoplay-button"
+            onClick={handleAutoplayToggle}
+          >
+            {autoplay ? "Pause" : "Play"}
+          </button>
+        </div>
       </div>
     </div>
   );
